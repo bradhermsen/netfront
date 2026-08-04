288fa1b92c30eb27513dbcaa2d1cd0fc79241943
bradhermsen <brad.hermsen@gmail.com>
2026-08-04 15:48:11 -0500

    updates yml file

--- NetFrontBridgeApp/obj/Debug/net10.0-windows/win-x64/NetFrontBridgeApp.AssemblyInfo.cs
+++ NetFrontBridgeApp/obj/Debug/net10.0-windows/win-x64/NetFrontBridgeApp.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("NetFrontBridgeApp")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+f28f43fabc2558f441cff828aec68bdd02164fa3")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+8cca943593e948f3d6f5a373704b5613a16e2e57")]
 [assembly: System.Reflection.AssemblyProductAttribute("NetFrontBridgeApp")]
 [assembly: System.Reflection.AssemblyTitleAttribute("NetFrontBridgeApp")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
